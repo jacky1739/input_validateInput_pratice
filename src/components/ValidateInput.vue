432c080db7f274460cc84a27e11d6a8b5e10b476
Jacky <jacky123789456@gmail.com>
2022-05-16 16:34:01 +0800

    validateinput 新增 switch

--- src/components/ValidateInput.vue
+++ src/components/ValidateInput.vue
@@ -11,7 +11,7 @@
 <script lang="ts">
 import { reactive, PropType, defineComponent } from 'vue'
 
-// const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
 interface RuleProp {
   type: 'required' | 'email'
@@ -33,14 +33,22 @@ export default defineComponent({
 
     const validateInput = () => { // 每個 rules 都必須通過 只要有一個沒有就視為 false 因此使用 every // every 會返回一個 boolean 值
       if (props.rules) {
-        // const allPassed = props.rules.every(rule => {
-        //   let passed = true
-        //   inputRef.message = rule.message
-        //   switch (rule.type) {
-        //     case 'required':
-        //       passed = (inputRef)
-        //   }
-        // })
+        const allPassed = props.rules.every(rule => {
+          let passed = true
+          inputRef.message = rule.message
+          switch (rule.type) {
+            case 'required':
+              passed = (inputRef.val.trim() === '')
+              break
+            case 'email':
+              passed = emailReg.test(inputRef.val)
+              break
+            default:
+              break
+          }
+          return passed
+        })
+        inputRef.error = !allPassed
       }
     }
     return { inputRef, validateInput }
