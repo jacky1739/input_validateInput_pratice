385e390d39c7dd2c24d2b81946b7b9d65bc14e8b
Jacky <jacky123789456@gmail.com>
2022-06-10 11:48:48 +0800

    新增PostList.vue

--- src/view/ColumnDetail.vue
+++ src/view/ColumnDetail.vue
@@ -9,7 +9,7 @@
         <p class="text-muted">{{column.description}}</p>
       </div>
     </div>
-    <post-list :list="list"></post-list>
+    <!-- <post-list :list="list"></post-list> -->
   </div>
 </template>
 
@@ -17,7 +17,11 @@
 import { defineComponent } from 'vue'
 import { useRoute } from 'vue-router'
 import { testData, testPosts } from '../testData'
+// import PostList from '../components/PostList.vue'
 export default defineComponent({
+  components: {
+    // PostList
+  },
   setup () {
     const route = useRoute()
     const currentId = +route.params.id // 利用+號把id從number轉為string
